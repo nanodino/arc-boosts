0afc0c3e4991b1fbff8a73eccb6954fdd557e7c2
Nancy <nancy@transit.app>
2024-07-05 08:02:43 -0400

    add dialog box for squash and merge

--- no-bad-decisions.js
+++ no-bad-decisions.js
@@ -3,7 +3,7 @@ function askForConfirmation() {
 }
 
 function mergePullRequest(event) {
-    if (!askForConfirmation()) {
+    if (isAfter3pm() && !askForConfirmation()) {
         event.preventDefault();
         console.log('Pull request not merged');
     } else {
@@ -20,11 +20,17 @@ function isAfter3pm() {
 }
 
 function main() {
-    // use the giant selector so the popup window does not reappear when clicking cancel
-    const mergeButton = document.querySelectordocument.querySelector('#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging.is-updating-via-merge > div > div > div > div > div.merge-message > div > div > button.merge-box-button.btn-group-merge.rounded-left-2.btn.btn-primary.BtnGroup-item.js-details-target.hx_create-pr-button');
-    if (mergeButton && isAfter3pm()) {
-        mergeButton.removeEventListener('click', mergePullRequest); 
+    // use the giant selectors so the popup window does not reappear when clicking cancel
+    const mergeButton = document.querySelector('#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging.is-updating-via-merge > div > div > div > div > div.merge-message > div > div > button.merge-box-button.btn-group-merge.rounded-left-2.btn.btn-primary.BtnGroup-item.js-details-target.hx_create-pr-button');
+    const squashButton = document.querySelector('#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-squashing.is-updating-via-merge > div.js-merge-message-container > div > div > div > div.merge-message > div > div > button.merge-box-button.btn-group-squash.rounded-left-2.btn.btn-primary.BtnGroup-item.js-details-target.hx_create-pr-button')
+     if(mergeButton){
+        confirm('Are you sure you don\'t want to SQUASH and merge here?')
+        mergeButton.removeEventListener('click', mergePullRequest);
         mergeButton.addEventListener('click', mergePullRequest);
+     }
+    if (squashButton && isAfter3pm()) {
+        squashButton.removeEventListener('click', mergePullRequest); 
+        squashButton.addEventListener('click', mergePullRequest);
     } else {
         console.log('Pull request not merged');
     }
